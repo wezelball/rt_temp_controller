8ae07dc99b5a5ef94693a9fafeb64071b9f48432
David Cohen <davelcohen2000@yahoo.com>
2014-11-06 05:59:28 -0500

    Graph now plots in a rectangular box of arbitrary size

--- rt_temp_controller.ino
+++ rt_temp_controller.ino
@@ -14,7 +14,6 @@
 #include "fonts/SystemFont5x7.h"   // system font
 #include "bitmaps/ArduinoIcon.h"   // bitmap 
 
-
 /* constants (thou art not evil) */
 const float pi = 3.1416;  // good ole pi
 const int SCREEN_WIDTH = 128;    // KS0108 LCD max screen width x
@@ -22,31 +21,36 @@ const int SCREEN_HEIGHT = 64;    // KS0108 LCD max screen height y
 
 /* global variables (thou art evil) */
 unsigned int x = 0;
-//float y = 0.0;
-float y[360];  // this is the y array
+float y[360];       // this is the y array
+int graphXMin = 30;  // the leftmost limit of the graph
+int graphXMax = 90;  // the rightmost limit of the graph
+int graphYMin = 20;  // the top limit of the graph
+int graphYMax = 50;  // the bottom limit of the graph
 
 void setup()  {
   GLCD.Init(NON_INVERTED);     // initialise the library, non inverted writes pixels onto a clear screen
   GLCD.ClearScreen();
   GLCD.DrawBitmap(ArduinoIcon, 32,0, BLACK); //draw the bitmap at the given x,y position
   GLCD.ClearScreen();
+  GLCD.DrawRect(graphXMin,graphYMin,(graphXMax-graphXMin),(graphYMax-graphYMin),BLACK);
   Serial.begin(9600);  // open the serial port for debugging
 }
 
 /* Converts actual X value to screen coordinate
- * for plotting. We'll start simple and plot
- * a sine function from y - 1 to -1 and 
- * x = 0 to 360
+ * for plotting. Returns value between
+ * graphXMin and graphXmax
  */
 int xToScreen(float x) {
-  return (int(x * (SCREEN_WIDTH/360.0)));
+  return (int(x * (graphXMax-graphXMin)/360.0 + graphXMin ));
 }
 
+/* Converts actual Y value to screen coordinate
+ * for plotting. Returns value between
+ * graphYMin and graphYMax
+ */
 int yToScreen(float y) {
-  return(int(SCREEN_HEIGHT/2 - y*SCREEN_HEIGHT/2));
+  return(int(graphYMin + (graphYMax-graphYMin)/2 - y*(graphYMax-graphYMin)/2));
 }
-
-
 
 void loop()
 {
@@ -62,6 +66,8 @@ void loop()
     {
       GLCD.ClearScreen();
       x = 0;
+      /* Draw the outer bounds of the graph area */
+      GLCD.DrawRect(graphXMin,graphYMin,(graphXMax-graphXMin),(graphYMax-graphYMin),BLACK);
     }
     
     delay(20);  //milliseconds
