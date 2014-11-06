2f71732cf640413bc3ea1bd8e58b6d3da00bca68
David Cohen <davelcohen2000@yahoo.com>
2014-11-05 20:59:12 -0500

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ rt_temp_controller
 ==================
 
 Radio Telescope Temperature Controller
+
+This is a homebrew but high-performance temperature controller
+for a single-dish radio telescope.
---
+++ rt_temp_controller.ino
@@ -0,0 +1,8 @@
+/* Dave Cohen
+ * An improved radio telescope temperature controller
+ * with a cool KS0108 LCD display and an Arduino Mega
+ * 2560 controller
+ * 11/5/2014
+ */
+ 
+ #include <glcd.h>
