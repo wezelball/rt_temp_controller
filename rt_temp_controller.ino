f3181299385b9680f82fe8f37d4f9bd4ef96b8a3
David Cohen <davelcohen2000@yahoo.com>
2014-11-08 19:19:36 -0500

    Improved header info, getting ready to add queue code, like to break my app

--- rt_temp_controller.ino
+++ rt_temp_controller.ino
@@ -12,7 +12,8 @@
 #include <glcd.h>
 #include "fonts/Arial14.h"         // proportional font
 #include "fonts/SystemFont5x7.h"   // system font
-#include "bitmaps/ArduinoIcon.h"   // bitmap 
+#include "bitmaps/ArduinoIcon.h"   // bitmap
+
 
 /* constants (thou art not evil) */
 const float pi = 3.1416;  // good ole pi
@@ -21,11 +22,14 @@ const int SCREEN_HEIGHT = 64;    // KS0108 LCD max screen height y
 
 /* global variables (thou art evil) */
 unsigned int x = 0;
-float y[360];       // this is the y array
-int graphXMin = 30;  // the leftmost limit of the graph
-int graphXMax = 90;  // the rightmost limit of the graph
-int graphYMin = 20;  // the top limit of the graph
-int graphYMax = 50;  // the bottom limit of the graph
+float y1[360];       // this is the channel 1 y array
+float y2[360];       // this is the channel 2 y array
+int graphXMin = 20;  // the leftmost limit of the graph
+int graphXMax = SCREEN_WIDTH-1;  // the rightmost limit of the graph
+int graphYMin = 10;  // the top limit of the graph
+int graphYMax = SCREEN_HEIGHT-1;  // the bottom limit of the graph
+float ch1Setpoint = 0.5;  // ch.1 temperature setpoint
+float ch2Setpoint = 0.5;  // ch.1 temperature setpoint
 
 void setup()  {
   GLCD.Init(NON_INVERTED);     // initialise the library, non inverted writes pixels onto a clear screen
@@ -39,16 +43,24 @@ void setup()  {
  * for plotting. Returns value between
  * graphXMin and graphXmax
  */
-int xToScreen(float x) {
-  return (int(x * (graphXMax-graphXMin)/360.0 + graphXMin));
+int xToScreen(float x, int screenXMin, int screenXMax) {
+  return (int(x * (screenXMax-screenXMin)/360.0 + screenXMin));
 }
 
 /* Converts actual Y value to screen coordinate
  * for plotting. Returns value between
  * graphYMin and graphYMax
  */
-int yToScreen(float y) {
-  return(int(graphYMin + (graphYMax-graphYMin)/2 - y*(graphYMax-graphYMin)/2));
+int yToScreen(float y, int screenYMin, int screenYMax) {
+  return(int(screenYMin + (screenYMax-screenYMin)/2 - y*(screenYMax-screenYMin)/2));
+}
+
+/*
+ * Prints current information line temperature, output,
+ * and setpoint to go at the top of the display
+ */
+void printCurrent() {
+  
 }
 
 void loop()
@@ -57,10 +69,21 @@ void loop()
   {
     if (x <= 360 )  
     {
-      y[x] = sin(x*pi/180);  // argument degrees converted to radians
-      GLCD.SetDot(xToScreen((float)x), yToScreen(y[x]), BLACK);
-      GLCD.CursorTo(0, 0);
-      GLCD.PrintNumber(x);
+      y1[x] = sin(x*pi/180);  // argument degrees converted to radians
+      y2[x] = cos(x*pi/180);
+      GLCD.SetDot(xToScreen((float)x,graphXMin,graphXMax), yToScreen(y1[x],graphYMin,graphYMax), BLACK);
+      GLCD.GotoXY(0, 0);
+      GLCD.Puts("Ch1: ");
+      GLCD.GotoXY(25,0);
+      GLCD.print(y1[x],1);
+      GLCD.GotoXY(50,0);
+      GLCD.Puts("SP: ");
+      GLCD.GotoXY(75,0);
+      GLCD.PrintNumber(ch1Setpoint);
+      GLCD.GotoXY(85,0);
+      GLCD.Puts("Out: ");
+      GLCD.GotoXY(110,0);
+      GLCD.PrintNumber(85);
       x++;
     }
     else
@@ -71,6 +94,6 @@ void loop()
       GLCD.DrawRect(graphXMin,graphYMin,(graphXMax-graphXMin),(graphYMax-graphYMin),BLACK);
     }
     
-    delay(20);  //milliseconds
+    delay(200);  //milliseconds
   }   
-}
+ }
